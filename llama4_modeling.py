c73bc21028cced4b5d99d6be03d50fc7b4d16150
amanimulira <amanimulira@gmail.com>
2025-07-10 16:22:04 +0100

    llama4text function - mixture of experts layer

--- llama4_modeling.py
+++ llama4_modeling.py
@@ -11,12 +11,100 @@ from transformers.models.llama4.configuration_llama4 import Llama4VisionConfig
 from activations import ACT2FN
 from cache_utils import Cache, DynamicCache
 from generation import GenerationMixin
-integrations.hub_kernels import use_kernel_forward_from_hub
-
-"""
-	`more imports`
-"""
+from integrations.hub_kernels import use_kernel_forward_from_hub
+from masking_utils import create_causal_mask, create_chunked_causal_mask
+from modeling_flash_attention_utils import FlashAttentionKwargs
+from modeling_layers import GradientCheckpointingLayer
+from modeling_outputs import BaseModelOutput, BaseModelOutputWithPast, CausalLMoutputWithPast, ModelOutput
+from modeling_rope_utils import ROPE_INIT_FUNCTIONS, dynamic_rope_update
+from modeling_utils import ALL_ATTENTION_FUNCTIONS, PreTrainedModel
+from processing_utils import Unpack
+from utils import TransformersKwargs, auto_docstring, can_return_tuple, logging
+from configuration_llama4 import Llama4Config, Llama4TextConfig
 
 logger = logging.get_logger(__name__)
 
+"""
+mixture of experts layer
 
+instead of having a single feed-forward network, there are multiple "expert" networks.
+
+a "gate" mechanism then determines which expert(s) should process each input.
+
+improving model capacity and efficiency.
+
+"""
+class Llama4Text(nn.Module):
+	def __init__(self, config: Llama4TextConfig):
+		super().__init__()
+		self.num_experts = config.num_local_experts
+		self.intermediate_size = config.intermediate_size
+		self.hidden_size = config.hidden_size
+		self.expert_dim = self.intermediate_size
+		self.gate_up_proj = nn.Parameter(torch.empty(self.num_experts, self.hidden_size, 2 * self.expert_dim))
+		self.down_proj = nn.Parameter(torch.empty((self.num_experts, self.expert_dim, self.hidden_size)))
+		self.act_fn = ACT2FN[config.hidden_act]
+        
+	def forward(self, hidden_states: torch.Tensor) -> torch.Tensor:
+		# (batch_size * sequence_length, hidden_size) -> sequence dims are flattened.
+		# (num_tokens, hidden_size), where num_tokens is the total number of tokens (batch_size * sequence_length)
+		
+		# reshapes input, which distributes the tokens among the experts.
+		# first dim explicitly represents different experts.
+		# -1 pytorch automatically calculates this dim, will be num_tokens / num_experts.
+		# each num_tokens_per_expert chunk of the orignial hidden_states is conceptually assigned to a specific expert.
+		# hidden_size - feature dim remains the same.
+
+		# This effectively "assigns" groups of input tokens to different experts.
+		# Performs a simple block wise assingment.
+
+		hidden_states = hidden_states.view(self.num_experts, -1, self.hidden_size)
+
+		# hidden_states -> (self.num_experts, total_tokens_per_expert, self.hidden_size)
+		# self.gate_proj -> (self.num_experts, self.hidden_size, 2 * self.expert_dim) 
+
+		# So then torch.bmm performs batch matrix multiplication, iterating thorugh the first dim.
+		# Performs matrix multipliatoin for each corresponding pair of "batches"
+		# gate_up -> (total_tokens_per_expert, 2 * expert_dim)
+
+		# This step applies the initial linear projection for both the "gate" and the "up projection" 
+		# components independently for each expert. We are going from hidden_size to 2 * expert_dim.
+		gate_up = torch.bmm(hidden_states, self.gate_up_proj)
+
+		# Spliting the gate_up tensor into two equal-sized chunks along the last dim.
+
+		# gate shape: (self.num_experts, total_tokens_per_expert, self.expert_dim)
+		# up shape: (self.num_experts, total_tokens_per_expert, self.expert_dim)
+
+		# Seperates the comnined projection into two distinct tensors
+
+		# gate, will control activation
+		# up projection, main transformed features
+		gate, up = gate_up.chunk(2, dim=-1) # not supported for DTensors
+
+		# self.act_fn(gate): applies non-linear activation funciotn element wise to the gate tensor. (shape preserved)
+		# up * self.act_fn(gate): element-wise multiplication (Hadamard product) between up and activated gate tensor.
+		# CORE OF GATING MECHANISM.
+
+		# self.down_proj Shape: (self.num_experts, self.expert_dim, self.hidden_size)
+
+		# torch.bmm, for each expert i:
+		# intermediate result: (up * self.act_fn(gate))[i]
+		# the shape -  (total_tokens_per_expert, expert_dim)
+
+		# self.down_proj[i] (shape expert_dim, hidden_size)
+
+		# Completes individual expert compution: the modulated features are transformed back to the model's hidden_size
+		# by the expert's specific down_proj weights.
+		next_states = torch.bmm((up * self.act_fn(gate)), self.down_proj)
+
+		# (self.num_experts, total_tokens_per_expert, self.hidden_size) -> (total_tokens, self.hidden_size)
+
+		# Restores the output to the original "flattened tokens" shape. 
+
+		# Effectively combines the outputs from all experts back into a single sequence of hidden states.
+		next_states = next_states.view(-1, self.hidden_size)
+
+		return next_states
+
+            
